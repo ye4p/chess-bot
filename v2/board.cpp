bcc133a4a7403c9317fc0f3f8ebdd1a1a70bf625
Danila Golubkov <danilgolubkov172@gmail.com>
2026-03-14 23:37:13 -0400

    fix

---
+++ v2/board.cpp
@@ -0,0 +1,9 @@
+#include <array>
+#include <vector>
+
+class Board
+{
+    std::array<uint64_t, 12> bitboards;
+    // 0-5 - white pieces. 0 - pawns, 1 - knights, 2 - bishops, 3 - rooks, 4 - queen, 5 - king
+    // 6-11 - black pieces. 6 - pawns, 7 - knights, 8 - bishops, 9 - rooks, 10 - queen, 11 - king
+};
