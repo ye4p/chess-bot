5cb708cab4a691382e04f9930490252e28f355a3
Danila Golubkov <danilgolubkov172@gmail.com>
2026-03-17 23:55:38 -0400

    fix

--- v2/board.cpp
+++ v2/board.cpp
@@ -11,11 +11,26 @@
 static const std::unordered_map<char, int> pieceMap =
     {
         {'p', 0},
-        {'k', 1},
+        {'n', 1},
         {'b', 2},
         {'r', 3},
         {'q', 4},
         {'k', 5}};
+
+struct Undo
+{
+    uint8_t castlingRights;
+    int8_t enPassantSquare;
+    uint8_t halfMoveClock;
+    uint64_t fullMoveClock;
+    Undo(uint8_t castlingRights, int8_t enPassantSquare, uint8_t halfMoveClock, uint64_t fullMoveClock)
+    {
+        this->castlingRights = castlingRights;
+        this->enPassantSquare = enPassantSquare;
+        this->halfMoveClock = halfMoveClock;
+        this->fullMoveClock = fullMoveClock;
+    }
+};
 
 class Board
 {
@@ -31,9 +46,20 @@ class Board
     int enPassantSquare = -1;
 
     int halfMoveClock = 0;
-    int fullMoveClock = 0;
+    int fullMoveClock = 1;
 
     bool whiteToMove = true;
+
+    Board()
+    {
+        bitboards.fill(0ULL);
+        occupancies.fill(0ULL);
+        uint8_t castlingRights = 0b0000;
+        int enPassantSquare = -1;
+        int halfMoveClock = 0;
+        int fullMoveClock = 1;
+        bool whiteToMove = true;
+    }
 
     //
     //      Helper Methods:
@@ -52,6 +78,30 @@ class Board
     {
         return (bitboards[index] & (1ULL << square)) > 0;
     }
+    uint64_t get_lsb(uint64_t bb)
+    {
+        // return (bb & ((~bb) + 1));
+        return __builtin_ctzll(bb);
+    }
+    uint64_t pop_lsb(uint64_t &bb)
+    {
+        int square = __builtin_ctzll(bb);
+        bb &= bb - 1;
+        return square;
+    }
+    inline int popcount(uint64_t bb)
+    {
+        // int count=0;
+        // while (bb) {
+        //     bb&=bb-1;
+        //     count++;
+        // }
+        return __builtin_popcount(bb);
+    }
+
+    //
+    // FEN
+    //
 
     std::vector<std::string> splitString(std::string str, char delimiter)
     {
@@ -94,11 +144,11 @@ class Board
 
         // Clear prev position:
         bitboards.fill(0ULL);
-        int8_t castlingRights = 0b0000;
-        int enPassantSquare = -1;
-        int halfMoveClock = 0;
-        int fullMoveClock = 0;
-        bool whiteToMove = true;
+        castlingRights = 0b0000;
+        enPassantSquare = -1;
+        halfMoveClock = 0;
+        fullMoveClock = 0;
+        whiteToMove = true;
 
         int i = 0;
         for (std::string row : rows)
@@ -112,44 +162,88 @@ class Board
                     int piece = pieceMap.at(letConverted);
                     int fullPiece = piece + color;
                     setBit(piece + color, i);
-                    if (std::isdigit(let))
-                    {
-                        int letConverted = let - '0';
-                        i += letConverted;
-                    }
+                    i++;
                 }
-            }
-            if (vec[1] == "w")
-            {
-                whiteToMove = true;
-            }
-            else
-            {
-                whiteToMove = false;
-            }
-
-            for (char let : vec[2])
-            {
-                char lower = std::tolower(let);
-                int shift = (std::isupper(let) ? 2 : 0);
-                if (lower == 'k')
+                if (std::isdigit(let))
                 {
+                    int letConverted = let - '0';
+                    i += letConverted;
                 }
-            }
-            int squareEnPassant = codeToIndex(vec[3]);
-            if (vec[3] == "-")
-            {
-                enPassantSquare = -1;
-            }
-            else
-            {
-                enPassantSquare = squareEnPassant;
-            }
-            if (vec.size() > 4)
-            {
-                halfMoveClock = std::stoi(vec[4]);
-                fullMoveClock = std::stoi(vec[5]);
             }
         }
+        if (vec[1] == "w")
+        {
+            whiteToMove = true;
+        }
+        else
+        {
+            whiteToMove = false;
+        }
+
+        for (char let : vec[2])
+        {
+            switch (let)
+            {
+            case 'K':
+            {
+                castlingRights |= (1 << 3);
+                break;
+            }
+            case 'Q':
+            {
+                castlingRights |= (1 << 2);
+                break;
+            }
+            case 'k':
+            {
+                castlingRights |= (1 << 1);
+                break;
+            }
+            case 'q':
+            {
+                castlingRights |= (1 << 0);
+                break;
+            }
+            }
+        }
+        int squareEnPassant = codeToIndex(vec[3]);
+        if (vec[3] == "-")
+        {
+            enPassantSquare = -1;
+        }
+        else
+        {
+            enPassantSquare = squareEnPassant;
+        }
+        if (vec.size() > 4)
+        {
+            halfMoveClock = std::stoi(vec[4]);
+            fullMoveClock = std::stoi(vec[5]);
+        }
+        updateOccupancies();
     }
+    void updateOccupancies()
+    {
+        occupancies[0] = 0ULL;
+        occupancies[1] = 0ULL;
+        occupancies[2] = 0ULL;
+        for (int i = 0; i <= 5; i++)
+        {
+            occupancies[0] |= bitboards[i];
+        }
+        for (int i = 6; i <= 11; i++)
+        {
+            occupancies[1] |= bitboards[i];
+        }
+        occupancies[2] = occupancies[0] | occupancies[1];
+    }
+
+    //
+    // Move representation
+    //
+
+    //
+    //  First 6 bits - FROM square, next 6 bits - TO square, 3 bits - piece type that just moved,
+    //  3 bits - piece type that got captured, 3 bits - promotion piece, and the rest 11 are for move flags
+    //
 };
